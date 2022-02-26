3386a1761b9c952a653c9822c3cda1e1c4543a23
Jose Omar Landa <79344240+omar-landa@users.noreply.github.com>
2022-02-26 15:23:23 -0600

    Update ArrayPractice.php
    added a for-loop to print the array values as well as the print _r command

--- ArrayPractice.php
+++ ArrayPractice.php
@@ -10,9 +10,19 @@ $paper[] = "Laser";
 $paper[] = "Photo";
 
 print_r($paper);
+echo "<br>";
+echo "<br>";
 
 /* Each value is assigned to an index in ascending order, with
 "Copier" being assigned to index [0], "Inket" to index [1], 
 and so on. */
+
+/* You can use a for loop to print out the array values
+instead of using print_r */
+
+for ($i = 0; $i < 4; $i++) {
+    echo "$i: $paper[$i]<br>";
+}
+
 
 ?>
