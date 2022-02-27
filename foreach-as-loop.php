733c87fe49900ba94f854d1f48593b61bd3db916
Jose Omar Landa <79344240+omar-landa@users.noreply.github.com>
2022-02-26 19:41:40 -0600

    Update foreach-as-loop.php
    Added the 'list' function with examples.

--- foreach-as-loop.php
+++ foreach-as-loop.php
@@ -34,6 +34,35 @@ foreach($paper as $item => $description)
   echo "$item: $description<br>"
 
 
+   
+   
+   
+   
+   
+   
+
+   
+   
+ /* The List Function:
+   The list function is an alternative to the foreach...as loop.
+   It takes an array as its argument and then assigns the values 
+   of the array to the variables inside the paranthesis.*/
+   $paper = array('copier' => "Copier and Multipurpose",
+   'inkjet' => "Inkjet Printer",
+   'laser' => "Laser Printer",
+   'photo' => "Photo Printer");
+
+   while (list($item, $description) = each($paper))
+    echo "$item: $description<br>";
+
+
+
+
+//Another list function example:
+list($a, $b) = array('Jose', 'Gina');
+echo "a=$a b=$b;"
+
+?>
 
 
 ?>
